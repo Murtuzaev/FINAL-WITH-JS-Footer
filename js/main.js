4dc9155f1d3e1a05f9caf173d17356222c8c1f0e
Ramo01 <r.murtuzayev@list.ru>
2022-04-28 13:46:01 +0400

    lp home

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let toTop=document.querySelector(".to-top");
+
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset > 200){
+        toTop.classList.add("active")
+    }
+    else{
+        toTop.classList.remove("active")
+    }
+})
